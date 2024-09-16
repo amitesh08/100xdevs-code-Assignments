1f6b491a9696aa881bd19659fbe937f89dbd695a
amitesh08 <aamitesh125@gmail.com>
2024-09-16 20:44:39 +0530

    ignore

--- week-8/paytm/frontend/src/App.jsx
+++ week-8/paytm/frontend/src/App.jsx
@@ -15,7 +15,6 @@ function App() {
           <Route path="/signup" element={<Signup/>}  />
           <Route path="/dashboard" element={<Dashboard/>}  />
           <Route path="/send" element={<SendMoney/>}  />
-          
         </Routes>
       </BrowserRouter>
     </>
