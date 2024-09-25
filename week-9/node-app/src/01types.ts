6c652ea9b09c8b8caa5356ee50ac56ae7ed751ce
amitesh08 <aamitesh125@gmail.com>
2024-09-25 21:49:52 +0530

    typescript p-2

---
+++ week-9/node-app/src/01types.ts
@@ -0,0 +1,17 @@
+//Explicit type - means you have to write the type 
+let fname: string = "amitesh";  // it is used to define strings.
+console.log(fname);
+
+//implicit type - compiler will guess the type 
+let fname2 = "amitesh";
+console.log(fname2);
+
+let fname3 : any = 55 //any type : in this case you can assign any value to var.
+console.log(fname3)
+fname3 = true
+console.log(fname3);
+//so if don't know what type of value are you gonna store in the var then use ANY.
+
+
+
+
