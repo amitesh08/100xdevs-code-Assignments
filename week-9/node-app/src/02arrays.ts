6d38a5e2539198a735e0fd4b14a12d2e827991a7
amitesh08 <aamitesh125@gmail.com>
2024-09-26 21:31:21 +0530

    ts-arrays

---
+++ week-9/node-app/src/02arrays.ts
@@ -0,0 +1,10 @@
+
+
+
+const arr1: readonly string[] = ["hello"] // read only means you can push or update values.
+// arr1.push("err")
+
+const arr: string[] = ["hi"]
+arr.push("amitesh");
+console.log(arr)
+
