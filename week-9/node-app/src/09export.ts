b8cb25654897789f62e7b1bd660297c2778ee3b5
amitesh08 <aamitesh125@gmail.com>
2024-10-01 22:25:03 +0530

    export import

---
+++ week-9/node-app/src/09export.ts
@@ -0,0 +1,9 @@
+//exporting 
+
+export function sum(a:number , b:number):number {
+    return a+b;
+}
+
+export function sub(a:number , b:number):number {
+    return a-b;
+}
---
+++ week-9/node-app/src/10import.ts
@@ -0,0 +1,6 @@
+//importing
+import { sub } from "./09export";
+import { sum } from "./09export";
+
+console.log(sum(1,2));
+console.log(sub(5,3));
