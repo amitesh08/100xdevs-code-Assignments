55c63dc13fa8192ce76274a9344d9e40ea1f9fd9
amitesh08 <aamitesh125@gmail.com>
2024-10-13 23:19:05 +0530

    postgres JOINS-1

---
+++ week-10/00postgres/src/joins/advance-1.ts
@@ -0,0 +1,21 @@
+import { getClient } from "../utils";
+
+
+async function getUserAndTodosWithJoin(userId: number){
+    const client = await getClient();
+    //this query will give you join result of todos and people table which having same userId.
+    const joinQuery = `
+        select people.*, todos.title, todos.description, todos.done 
+        from people 
+        left join todos on people.id = todos.user_id 
+        where people.id = $1
+    `;
+
+    let res = await client.query(joinQuery, [userId])
+    const result = res.rows;
+
+    console.log(`user and todos: ${JSON.stringify(result)}`)  // it convert result into array and show it to user.
+
+}
+
+getUserAndTodosWithJoin(3);
