14036188908658d2e4927ca24f69d4a97464ef19
amitesh08 <aamitesh125@gmail.com>
2024-12-03 13:04:54 +0530

    js revision-1

---
+++ revision-2/Day-1/Day-1.js
@@ -0,0 +1,15 @@
+// Day- 1 (let, const, var , functions)
+
+const a = 10; 
+
+let b = 12;
+
+var c = 34;
+function test(){
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+
+test();
